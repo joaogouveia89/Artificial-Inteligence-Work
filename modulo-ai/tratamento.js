a95e764091079f22b959028ae959eeadd9694be0
Joao Gouveia <j.lucas.gouveia@gmail.com>
2016-11-19 13:30:38 -0200

    inteligencia melhor curso

--- modulo-ai/tratamento.js
+++ modulo-ai/tratamento.js
@@ -10,6 +10,10 @@ module.exports = function(mensagem_usuario){
   }
 
   if(mensagem_usuario.indexOf("voce") !== -1 && (mensagem_usuario.indexOf("sobre") || mensagem_usuario.indexOf("quem é"))){
-    return "Sou um robo construido pelos alunos Joao Gouveia, Alexandre Pirfo e Ramirez Gonçalves para a disciplina de inteligencia artificial na FUMEC. Posso te ajudar com duvidas relacionadas à viagens";
+    return "Sou um robo construido pelos alunos Joao Gouveia, Alexandre Pirfo e Ramirez Gonçalves para a disciplina de inteligencia artificial na FUMEC. Posso te ajudar com duvidas relacionadas à FUMEC e os cursos que tem la";
+  }
+
+  if(mensagem_usuario.indexOf("melhor") !== -1 && mensagem_usuario.indexOf("curso") !== -1){
+    return "O melhor curso da fumec com certeza é o de ciência da computação :p";
   }
 }
