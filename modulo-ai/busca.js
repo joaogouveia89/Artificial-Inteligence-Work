fe773ec162b3eb6eca87c8aa642400f25c732ccd
Joao Gouveia <j.lucas.gouveia@gmail.com>
2016-11-20 22:50:48 -0200

    correcao de bugs

--- modulo-ai/busca.js
+++ modulo-ai/busca.js
@@ -63,9 +63,9 @@ module.exports = function(mensagem_usuario, dados){
       if(unidade === "face"){
         dados.unidades.face.cursos.forEach(function(c){
           resposta+=c;
-          if(dados.unidades.face.cursos[dados.unidades.face.cursos.length - 1 !== c){
+          if(dados.unidades.face.cursos[dados.unidades.face.cursos.length - 1] !== c){
             resposta+", ";
-          }          
+          }
         });
       }else if(unidade === "fea"){
         dados.unidades.fea.cursos.forEach(function(c){
